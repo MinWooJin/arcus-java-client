03abdbbe40f8d78533bcd660a4885ab17cae6633
MinWooJin <alsdn653@jam2in.com>
2018-05-23 09:30:13 +0900

    refactoring with review of @minkikim89

--- src/main/java/net/spy/memcached/MigrationMonitor.java
+++ src/main/java/net/spy/memcached/MigrationMonitor.java
@@ -25,7 +25,6 @@ import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MigrationMonitor extends SpyObject {
